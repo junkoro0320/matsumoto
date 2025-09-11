a3e8750185346c28d9590aed9227019745504f01
Junkoro320 <yanagidajunko0320@gmail.com>
2025-09-11 09:39:06 +0900

    育てているみかんたち作成

--- js/slider.js
+++ js/slider.js
@@ -1,26 +1,31 @@
-$('slider').slick({
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 0,
-    speed: 6900,
-    infinite: true,
-    pauseOnHover: false,
-    pauseOnFocus: false,
-    cssEase: 'linear',
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-        {
-            breakpoint: 769,
-            settinngs: {
-                slideToShow: 2,
+$(document).ready(function () {
+    $('.slider--frame').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 6900,
+        infinite: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        cssEase: 'linear',
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                    variableWidth: true
+                }
+            },
+            {
+                breakpoint: 426,
+                settings: {
+                    slidesToShow: 1,
+                    variableWidth: true
+                }
             }
-        },
-        {
-            breakpoint: 426,
-            settings: {
-                slidesTOshow: 1.5,
-            }
-        }]
-
+        ]
+    });
 });
