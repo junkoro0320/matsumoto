9bd775bf6af0fe780e764fdd5ef1432f70f046df
Junkoro320 <yanagidajunko0320@gmail.com>
2025-09-13 20:47:19 +0900

    スライダーアニメーション

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // ===== fade-in
     const targets = document.querySelectorAll(".fade-in-section");
-
     const observer = new IntersectionObserver((entries, obs) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("visible");
-                obs.unobserve(entry.target); // 1回だけ実行
+                obs.unobserve(entry.target);
             }
         });
-    }, { threshold: 0.2 }); // 20%見えたら実行
-
+    }, { threshold: 0.2 });
     targets.forEach(target => observer.observe(target));
+
+
 });
+
+
+
+window.addEventListener("load", () => {
+    const loading = document.getElementById("loading");
+    const waveContainer = loading.querySelector(".wave-container");
+    // 5秒後にフェードアウト
+    setTimeout(() => {
+        loading.style.opacity = "0"; // 透明にする
+
+        // 透明になったら display:none にする
+        setTimeout(() => {
+            loading.style.display = "none";
+        }, 1000); // CSSの transition時間と合わせる
+    }, 5000);
+});
+
+
+
