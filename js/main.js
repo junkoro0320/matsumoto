b07446527a4bc29acbe2ed3f11f311c347e3238d
Junkoro320 <yanagidajunko0320@gmail.com>
2025-09-14 20:46:40 +0900

    モーダルウィンドウ

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+
+
+// フェードインセクションの設定
+
 document.addEventListener("DOMContentLoaded", () => {
     // ===== fade-in
     const targets = document.querySelectorAll(".fade-in-section");
@@ -10,25 +14,60 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }, { threshold: 0.2 });
     targets.forEach(target => observer.observe(target));
-
-
 });
 
 
+// ローディング画面フェードアウト
 
 window.addEventListener("load", () => {
     const loading = document.getElementById("loading");
     const waveContainer = loading.querySelector(".wave-container");
-    // 5秒後にフェードアウト
+    // 4秒後にフェードアウト
     setTimeout(() => {
-        loading.style.opacity = "0"; // 透明にする
+        loading.style.opacity = "0";
 
         // 透明になったら display:none にする
         setTimeout(() => {
             loading.style.display = "none";
-        }, 1000); // CSSの transition時間と合わせる
-    }, 5000);
+        }, 1000); // CSSの transition時間１Sと合わせる
+    }, 4500);
 });
+
+
+// モーダル機能
+const modalTriggers = document.querySelectorAll('.modal-trigger');
+const modalCloseBtns = document.querySelectorAll('.modal__close-btn');
+modalTriggers.forEach(trigger => {
+    trigger.addEventListener('click', () => {
+        const modalId = trigger.dataset.modalId;
+        const targetModal = document.getElementById(modalId);
+
+
+        if (targetModal) {
+            targetModal.showModal();
+        }
+    });
+});
+
+modalCloseBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const targetModal = btn.closest('.modal');
+        if (targetModal) {
+            targetModal.close();
+        }
+    });
+});
+
+const allModals = document.querySelectorAll('.modal');
+allModals.forEach(modal => {
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.close();
+        }
+    });
+});
+
+
 
 
 
